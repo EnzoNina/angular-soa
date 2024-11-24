95922260dbcc212cfade80271419b9a5993791da
Enzo Nina <ninaenzo70@gmail.com>
2024-11-24 13:30:47 -0500

    add productos

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { VerifyEmailComponent } from './verify-email/verify-email.component';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
+  declarations: [
+
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule,
-    LoginComponent
-  ]
+    LoginComponent,
+    RegisterComponent,
+    VerifyEmailComponent
+  ],
+  providers: [provideHttpClient()]
 })
 export class AuthModule { }
--- src/app/products/home-page/home-page.component.ts
+++ src/app/products/home-page/home-page.component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [CurrencyPipe, NgFor],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
+  products = [
+    {
+      id: 1,
+      name: 'Producto 1',
+      price: 19.99,
+      image: 'https://via.placeholder.com/250',
+    },
+    {
+      id: 2,
+      name: 'Producto 2',
+      price: 29.99,
+      image: 'https://via.placeholder.com/250',
+    },
+    {
+      id: 3,
+      name: 'Producto 3',
+      price: 39.99,
+      image: 'https://via.placeholder.com/250',
+    },
+    {
+      id: 4,
+      name: 'Producto 4',
+      price: 49.99,
+      image: 'https://via.placeholder.com/250',
+    },
+  ];
 
+  addToCart(product: any) {
+    console.log('Producto agregado al carrito:', product);
+    // Aquí puedes conectar con tu servicio del carrito.
+  }
 }
